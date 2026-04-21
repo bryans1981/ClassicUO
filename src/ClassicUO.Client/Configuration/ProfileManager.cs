3808d8ba1c06d99c3859ef5e1c19459f69d01bfa
bryans1981 <bryans1981@gmail.com>
2026-04-21 12:48:37 -0400

    Relax browser profile size validation

--- src/ClassicUO.Client/Configuration/ProfileManager.cs
+++ src/ClassicUO.Client/Configuration/ProfileManager.cs
@@ -89,12 +89,12 @@ namespace ClassicUO.Configuration
                 throw new InvalidDataException();
             }
 
-            if (profile.WindowClientBounds.X < 600)
+            if (!PlatformHelper.IsBrowser && profile.WindowClientBounds.X < 600)
             {
                 profile.WindowClientBounds = new Point(600, profile.WindowClientBounds.Y);
             }
 
-            if (profile.WindowClientBounds.Y < 480)
+            if (!PlatformHelper.IsBrowser && profile.WindowClientBounds.Y < 480)
             {
                 profile.WindowClientBounds = new Point(profile.WindowClientBounds.X, 480);
             }
